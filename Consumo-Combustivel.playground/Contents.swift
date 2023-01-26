4c714bb6ff4b6791f0fbb029651df92d73fdada2
Marcelo Henrique <celohenriquech7@gmail.com>
2023-01-25 23:00:12 -0300

    estrutura basica

--- Consumo-Combustivel.playground/Contents.swift
+++ Consumo-Combustivel.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//- Quantos quilômetros roda com 1L X
+
+
+//- Quantos quilômetros roda com tanque cheio
+
+//- Quantos litros consome rodando 300 km
+
+//- Quantas vezes preciso encher o tanque para percorrer 1300 km
+
+//- Quanto custa encher o tanque
+
+//- Quanto custa a viagem de 671km
+
+let tanqueTotalEmLitro: Double = 42
+
+let tanqueAtual: Double = 5
+
+let kmRodadosAteAgr: Double = 330
+
+let tipoDeCombustivel = "Gasolina"
+
+let valorAtualGasolina = 5.72
+
+let consumoDeCombustivelAtual = tanqueTotalEmLitro - tanqueAtual
+
+func kmPerLiter(km: Double, liter: Double) -> Double {
+    return km / liter
+}
+
+print(kmPerLiter(km: kmRodadosAteAgr, liter: consumoDeCombustivelAtual))
+
+
+
+
